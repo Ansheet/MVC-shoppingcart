d9dabe5ec65d354f4a2e815c95bd21d42e548470
Vishlesh <ankur.patni@civica@co.uk>
2019-09-25 18:30:58 +0530

    Added Test Project

---
+++ OnlineShoppingCart.unitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OnlineShoppingCart.unitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OnlineShoppingCart.unitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6aa4b112-97ac-4a04-bcc9-fa2e71d722cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ OnlineShoppingCart.unitTesting/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineShoppingCart;
+using OnlineShoppingCart.Controllers;
+
+
+namespace OnlineShoppingCart.unitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Contact()
+        {
+            HomeController hc = new HomeController();
+            ViewResult vr = hc.Contact() as ViewResult;
+            Assert.IsNotNull(vr);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            HomeController hc = new HomeController();
+            ViewResult vr = hc.About() as ViewResult;
+            Assert.IsNotNull(vr);
+        }
+    }
+}
